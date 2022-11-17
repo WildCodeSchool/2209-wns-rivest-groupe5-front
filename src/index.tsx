f8d5a68030904511edd00f0ee60be41e9ccc1eb7
Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
2022-11-17 10:08:26 +0100

    add authlink in indexts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
 import ReactDOM from 'react-dom/client';
+import { setContext } from "@apollo/client/link/context";
 import './index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:5050/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5050/',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
