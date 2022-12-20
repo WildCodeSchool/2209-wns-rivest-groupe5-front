17963bfbd3766595e9134d9640d912ece273efd2
QuentD36 <82884437+QuentD36@users.noreply.github.com>
2022-12-20 17:35:53 +0100

    Build homepage, register page and login page (#10)
    * mep mui login & register
    
    * last update

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import {Link} from 'react-router-dom';
+import {AppBar, Box, Typography, Button} from '@mui/material';
+
+export default function Header() {
+  
+
+  const rightLink = {
+    fontSize: 16,
+    color: 'common.white',
+    ml: 3,
+  };
+
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'rgba(0,0,0,0.87)',
+  };
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar sx={{justifyContent: 'space-between'}}>
+        <Box sx={{flex: 1}} />
+        <Link to={'/'} style={linkStyle}>
+          <Typography fontSize={20}>WildCarbon</Typography>
+        </Link>
+        <Box sx={{flex: 1, display: 'flex', justifyContent: 'flex-end'}}>
+          <Button
+            color="secondary"
+            disabled={false}
+            size="medium"
+            variant="outlined"
+            component={Link}
+            to="/login"
+          >
+            Se connecter
+          </Button>
+          <Button
+            color="secondary"
+            disabled={false}
+            size="medium"
+            variant="contained"
+            sx={{ml: 2}}
+            component={Link}
+            to="/register"
+          >
+            S'inscrire
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,37 @@
-import Navbar from "../components/Navbar";
+import {
+  Button,
+  Stack,
+  CssBaseline,
+  Grid,
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Avatar,
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from 'react-router-dom';
+import * as React from 'react';
+import Header from '../components/Header';
+import FirstPart from '../components/FirstPart';
 
-const HomePage = () =>{
-    return (
-      <div>
-        Home Page
-      </div>
-    );
-}
+const HomePage = () => {
 
-export default HomePage
+  return (
+    <div>
+      <Header />
+      <FirstPart />
+    </div>
+  );
+};
+
+export default HomePage;
