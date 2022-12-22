aa9bc9acee2de09417f75d3072688591c56da82f
Vanessa <salvadoribanez.vanessa@gmail.com>
2022-12-22 16:43:13 +0100

    Created a theme with a custom palette

---
+++ src/assets/Styles/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#60e5dc",
+      main: "#17b2aa",
+      dark: "#00827b",
+      contrastText: "#000",
+    },
+    secondary: {
+      light: "#376775",
+      main: "##003c49",
+      dark: "#001721",
+      contrastText: "#fff",
+    },
+  },
+});
