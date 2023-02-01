c11187445e3594ebae04817536b7b49993fd2f26
Witz Kevin <90613127+witzkvn@users.noreply.github.com>
2023-02-01 16:26:04 +0100

    WCS-91B: doughnut sum chart (#19)
    * Wcs 8 feature create current user context recoil (#16)
    
    * current User context via recoil wip
    
    * user context via recoil wip
    
    Co-authored-by: Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
    
    * added pie chart for global emissions
    
    * cleaning
    
    ---------
    
    Co-authored-by: Razafinjatovo-dev <62689273+Razafinjatovo-dev@users.noreply.github.com>
    Co-authored-by: Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>

---
+++ src/components/carbonGraph/pieGraph/CarbonGraphSums.tsx
@@ -0,0 +1,59 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { useQuery } from "@apollo/client";
+import { GET_TOTAL_SUMS_ACTIVITIES_GRAPH_DATA } from "../../../graphql/queries/carbonGraphs/getTotalSumsActivitiesGraphData";
+import { pieChartOptions } from "./pieChartOptions";
+import styles from "./PieChart.module.css";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
+
+const CarbonGraphSums = () => {
+  const { data, loading, error } = useQuery(
+    GET_TOTAL_SUMS_ACTIVITIES_GRAPH_DATA
+  );
+
+  if (loading) return <div>Chargement...</div>;
+
+  if (error) return <div>Une erreur est survenue :{error.message}</div>;
+
+  return (
+    <div>
+      <Doughnut
+        options={pieChartOptions}
+        data={data.getMyTotalCarbonPerActivityType}
+      />
+      <div className={styles.textWrapper}>
+        <h2 className={styles.title}>Total (depuis toujours)</h2>
+        <div>
+          <div className={styles.quantity}>
+            {data.getMyTotalCarbonPerActivityType.datasets[0].data.reduce(
+              (acc: number, curr: number) => acc + curr,
+              0
+            )}{" "}
+            kg
+          </div>
+          <div>de CO2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarbonGraphSums;
