14ad2dae3c22979029bd78bd802c3d49c4518141
Razafinjatovo-dev <62689273+Razafinjatovo-dev@users.noreply.github.com>
2023-03-03 11:57:16 +0100

    Nested routes layout (#41)
    * goodDealsFeed wip
    
    * create common layout
    
    * fix conflict in MenuAccount tsx
    
    * include activity list below graphs
    
    * remove old dashboard layout and page
    
    * add date fns lib
    
    ---------
    
    Co-authored-by: Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>

--- front/src/pages/GoodDealsFeed.tsx
+++ front/src/pages/GoodDealsFeed.tsx
@@ -1,10 +1,90 @@
-import { useRecoilValue } from "recoil";
-import { currentUserState } from "../atom/currentUserAtom";
+import {useRecoilValue} from 'recoil';
+import {currentUserState} from '../atom/currentUserAtom';
+import {useQuery} from '@apollo/client';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import {CardContent, Typography} from '@mui/material';
+import {useEffect, useState} from 'react';
+import {GET_ALL_GOOD_DEALS} from '../graphql/queries/goodDeals/getAllGoodDeals';
+import {IGoodDeal} from '../interfaces/goodDeals/IGoodDeal';
+import { format } from 'date-fns';
+import Button from '@mui/material/Button';
 
 const GoodDealsFeed = () => {
   const currentUser = useRecoilValue(currentUserState);
-  console.log(">>>>curr user>>>", currentUser);
-  return <div>Good Deals Feed</div>;
+  const url = require('../assets/default-placeholder.png');
+    
+  const {data, error, loading} = useQuery(GET_ALL_GOOD_DEALS, {
+    fetchPolicy: 'no-cache',
+  });
+  
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error ...</div>
+  }
+
+
+  return (
+    <Container maxWidth="xl">
+      <Typography variant='h3'>Feed Good Deals</Typography>
+      {data.getAllGoodDeals
+        .sort(
+          (a: any, b: any) =>
+            new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+        )
+        .map((e: any) => {
+          return (
+            <Card
+              style={{
+                backgroundColor: '#e7e7e7',
+                marginBottom: 25,
+              }}
+              sx={{
+                mt: 5,
+              }}
+              key={e.goodDealId}
+            >
+              <CardContent className="wc-flex">
+                {e.image && (
+                  <div>
+                    <img
+                      src={e.image}
+                      alt=""
+                      className="wc-image-gooddeals"
+                    ></img>
+                  </div>
+                )}
+                {!e.image && (
+                  <div>
+                    <img src={url} alt="" className="wc-image-gooddeals"></img>
+                  </div>
+                )}
+                <div className="ml-15 w-75">
+                  <h3>
+                    {e.goodDealId}. {e.goodDealTitle}
+                  </h3>
+                  <p>
+                    <span style={{fontSize: 12}}>
+                      {format(new Date(e.createdAt), 'dd/MM/yyyy')}{' '}
+                    </span>
+                  </p>
+                  <p style={{fontWeight: 'bolder'}}>
+                    {e.user.firstname} {e.user.lastname}
+                  </p>
+                  <p>{e.goodDealContent}</p>
+                </div>
+                <div className='btn-container'>
+                  <Button variant="contained">Voir le deal</Button>
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+    </Container>
+  );
 };
 
 export default GoodDealsFeed;
