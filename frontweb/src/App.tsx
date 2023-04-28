8ff9a31be49212070bd2227a60e05baac2974d6d
Vanesa Salvador <46045495+vanessacode@users.noreply.github.com>
2023-04-28 10:45:03 +0200

    WCS-122: delete activity from activity list with modal confirmation (#64)
    * WCS-122: delete activity from activity list with modal confirmation
    
    * Fix

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import PrivateRoutes from "./components/PrivateRoute";
@@ -13,80 +13,86 @@ import ProfilePage from "./pages/ProfilePage";
 import RegisterPage from "./pages/RegisterPage";
 import { currentUserState } from "./atom/currentUserAtom";
 import { useRecoilState } from "recoil";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import ResetPasswordStepOnePage from "./pages/ResetPasswordStepOnePage";
 import ResetPasswordStepTwoPage from "./pages/ResetPasswordStepTwoPage";
 import LayoutRoot from "./layout/LayoutRoot";
-import CarbonGraphs from "./pages/CarbonGraphs/CarbonGraphs";
+import Dashboard from "./pages/dashboard/Dashboard";
 import FollowedUsersActivitiesList from "./pages/FollowedUsersAcitivities";
-import MyActivitiesPage from "./pages/MyActivitiesPage";
 import PublicLayout from "./layout/PublicLayout";
-import {theme} from './assets/Styles/theme'
+import { theme } from "./assets/Styles/theme";
+import AllActivityListPage from "./pages/AllActivityListPage";
 
 function App() {
-    const [user, setUser] = useRecoilState(currentUserState);
-    console.log(">>>>Current user >>>", user);
-    useEffect(() => {
-        const currentUserInLocalStorage = JSON.parse(
-            localStorage.getItem("user") || "{}"
-        );
-        setUser(currentUserInLocalStorage);
-    }, []);
-
-    return (
-      <div>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <Routes>
-              {/* Public routes */}
-              <Route element={<PublicLayout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/contribution" element={<Contribution />} />
-                <Route
-                  path="/resetPassword/stepOne"
-                  element={<ResetPasswordStepOnePage />}
-                />
-                <Route
-                  path="/reset-password/*"
-                  element={<ResetPasswordStepTwoPage />}
-                />
-              </Route>
-
-              {/* Routes accessibles in public and in private, only layout around changes */}
-              {user && Object.keys(user).length !== 0 ? (
-                <Route element={<LayoutRoot />}>
-                  <Route path="/profile/:userId" element={<ProfilePage />} />
-                  <Route path="/good-deals-feed" element={<GoodDealsFeed />} />
-                </Route>
-              ) : (
-                <Route element={<PublicLayout />}>
-                  <Route path="/profile/:userId" element={<ProfilePage />} />
-                  <Route path="/good-deals-feed" element={<GoodDealsFeed />} />
-                </Route>
-              )}
-
-              <Route element={<LayoutRoot />}>
-                <Route element={<PrivateRoutes />}>
-                  <Route path="/dashboard" element={<CarbonGraphs />} />
-                  <Route path="/my-account" element={<MyAccount />} />
-                  <Route path="/admin" element={<AdminPage />} />
-                  <Route
-                    path="/followed-activities-feed"
-                    element={<FollowedUsersActivitiesList />}
-                  />
-                  <Route path="/my-activities" element={<MyActivitiesPage />} />
-                  <Route path="/good-deals-form" element={<GoodDealsForm />} />
-                  <Route path="/my-account" element={<MyAccount />} />
-                  <Route path="/profile/:userId" element={<ProfilePage />} />
-                </Route>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
-      </div>
+  const [user, setUser] = useRecoilState(currentUserState);
+  console.log(">>>>Current user >>>", user);
+  useEffect(() => {
+    const currentUserInLocalStorage = JSON.parse(
+      localStorage.getItem("user") || "{}"
     );
+    setUser(currentUserInLocalStorage);
+  }, []);
+
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            {/* Public routes */}
+            <Route element={<PublicLayout />}>
+              <Route path="/" element={<HomePage />} />
+
+              <Route path="/contribution" element={<Contribution />} />
+              <Route
+                path="/resetPassword/stepOne"
+                element={<ResetPasswordStepOnePage />}
+              />
+              <Route
+                path="/reset-password/*"
+                element={<ResetPasswordStepTwoPage />}
+              />
+            </Route>
+
+            {/* Routes accessibles in public and in private, only layout around changes */}
+            {user && Object.keys(user).length !== 0 ? (
+              <Route element={<LayoutRoot />}>
+                <Route path="/profile/:userId" element={<ProfilePage />} />
+                <Route path="/good-deals-feed" element={<GoodDealsFeed />} />
+              </Route>
+            ) : (
+              <Route element={<PublicLayout />}>
+                <Route path="/profile/:userId" element={<ProfilePage />} />
+                <Route path="/good-deals-feed" element={<GoodDealsFeed />} />
+              </Route>
+            )}
+
+            {/* Private routes only */}
+            <Route element={<LayoutRoot />}>
+              <Route element={<PrivateRoutes />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/my-account" element={<MyAccount />} />
+                <Route path="/admin" element={<AdminPage />} />
+                <Route
+                  path="/followed-activities-feed"
+                  element={<FollowedUsersActivitiesList />}
+                />
+                <Route
+                  path="/my-activities"
+                  element={<AllActivityListPage />}
+                />
+                <Route path="/good-deals-form" element={<GoodDealsForm />} />
+                <Route path="/my-account" element={<MyAccount />} />
+                <Route path="/profile/:userId" element={<ProfilePage />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
