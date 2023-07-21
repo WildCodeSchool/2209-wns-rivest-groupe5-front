08873e62fafc7c8e6f635baa3de04d373ea0b2d4
joel-razafi <62689273+Razafinjatovo-dev@users.noreply.github.com>
2023-07-21 10:54:36 +0200

    same design for cards abo activities  and my activities (#101)
    Co-authored-by: Joel Razafi <raza.joel.dev@gmail.com>

--- frontweb/src/pages/FollowedUsersAcitivities.tsx
+++ frontweb/src/pages/FollowedUsersAcitivities.tsx
@@ -87,8 +87,13 @@ const FollowedUsersActivitiesList = () => {
             return (
               <Card
                 style={{
-                  backgroundColor: '#e7e7e7',
-                  marginBottom: 25,
+                  backgroundColor: '#FFFFFF',
+                  marginTop: 15,
+                  marginBottom: 40,
+                  borderRadius: 10,
+                  boxShadow:
+                    '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+                  padding: 20,
                 }}
                 key={index}
               >
